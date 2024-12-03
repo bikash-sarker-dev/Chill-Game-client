91ba969c628adac1c7f7fb8b28ed0538ed4ed958
bikash sarker <bikash.webdeveloper5@gmail.com>
2024-12-04 01:52:26 +0600

    footer part add in project

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 
 const Root = () => {
@@ -7,6 +8,7 @@ const Root = () => {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
